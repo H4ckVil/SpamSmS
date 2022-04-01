d4e4b8dd13c037d6b7ba474e79cd729cb9e498a9
H4ckVil997 <102341702+H4ckVil@users.noreply.github.com>
2022-04-01 15:42:04 +0800

    Add files via upload

---
+++ SpamSmS.py
@@ -0,0 +1,24 @@
+import requests
+import time
+
+
+#Logo•
+print("""
+                33[36;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+                ;       S P A M  S M S %      ;33[31;1m
+                ;---------------------------;33[32;1m
+                33[0;32m;       Author : H4cVil     ;
+                ;---------------------------;33[32;1m
+                ;       WA : 082393056023    ;
+                33[36;1m;---------------------------;
+                ;       github : H4ckVil     ;33[32;1m
+                33[32;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+print ('\n[×] Nomor Di Awali dengan 8++')
+
+#run nomor
+nomor = input('[×] Normor Target : ')
+req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor}).text
+if 'Terkirim' in req:
+    print ('[√] SpamSmS Berhasil [√] ')
+else:
+    print ('[!] SpamSmS Gagal [!] ')

d0c1aaee548ccb9070849798baec486be9c6944e
H4ckVil997 <102341702+H4ckVil@users.noreply.github.com>
2022-04-02 11:30:30 +0800

    Update SpamSmS.py

--- SpamSmS.py
+++ SpamSmS.py
@@ -2,20 +2,20 @@ import requests
 import time
 
 
-#Logo
-print(
-                33[36;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-                ;       S P A M  S M S %      ;33[31;1m
-                ;---------------------------;33[32;1m
-                33[0;32m;       Author : H4cVil     ;
-                ;---------------------------;33[32;1m
-                ;       WA : 082393056023    ;
-                33[36;1m;---------------------------;
-                ;       github : H4ckVil     ;33[32;1m
-                33[32;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;')
+# Logo
+
+print                33[36;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+print               ;       S P A M  S M S %      ;33[31;1m
+print                ;---------------------------;33[32;1m
+print                33[0;32m;       Author : H4cVil     ;
+print                ;---------------------------;33[32;1m
+print                ;       WA : 082393056023    ;
+print                33[36;1m;---------------------------;
+print                ;       github : H4ckVil     ;33[32;1m
+print ('               33[32;1m;;;;;;;;;;;;;;;;;;;;;;;;;;;;;')
 print ('\n[×] Nomor Di Awali dengan 8++')
 
-#run nomor
+# run nomor
 nomor = input('[×] Normor Target : ')
 req=requests.get("https://ainxbot-sms.herokuapp.com/api/spamsms",params={"phone":nomor}).text
 if 'Terkirim' in req:
